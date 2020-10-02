a27173f2d14ead5856309b053d947e7ab4f1db07
Kristine <kristine.ferg@gmail.com>
2020-10-02 12:15:21 -0400

    changed where main.py adds data history to

--- RaspberryPI/main.py
+++ RaspberryPI/main.py
@@ -7,32 +7,33 @@ from scp import SCPClient
 #os.system('sudo mount -a')
 
 #import argparse
-comport = '/dev/ttyACM0' #location need to be changed
+comport = '/dev/ttyACM0' #Arduino serial may need to be changed
 Time = datetime.datetime.now()
 Date = Time.isoformat()[0:10]
 
-filename = '/home/pi/Documents/Research/'+ Date + '.txt'
+filename = '/home/pi/Documents/Research/DataHistory/'+ Date + '.txt'
 #print(filename)
 if (not os.path.exists(filename)):
     with open(filename,'a') as f:
-        f.write('Time,CO2,PM01,PM2.5,PM10,tempf,tempc,hum,TGS_1,C7H8,H2S,NH3,C2H5OH,HCHO,O3_1,O3\n')
+        f.write('Time,CO2,PM01,PM2.5,PM10,tempf,hum,H2S,C7H8,C2H5OH,NH3\n')
 with serial.Serial( comport , 9600, timeout = 0.001) as arduino:
     while 1:
         if ( arduino.in_waiting > 116):
             Time = datetime.datetime.now()
             if (Time.isoformat()[0:10] != Date):
                 Date = Time.isoformat()[0:10]
-                filename = '/home/pi/Documents/Research/'+ Date + '.txt'
+                filename = '/home/pi/Documents/Research/DataHistory/'+ Date + '.txt'
                 with open(filename,'a') as f:
-                    f.write('Time,CO2,PM01,PM2.5,PM10,tempf,tempc,hum,TGS_1,C7H8,H2S,NH3,C2H5OH,HCHO,O3_1,O3\n')
+                    f.write('Time,CO2,PM01,PM2.5,PM10,tempf,hum,H2S,C7H8,C2H5OH,NH3\n')
             line = arduino.readline()
-            filename = '/home/pi/Documents/Research/'+ Date + '.txt'
+            filename = '/home/pi/Documents/Research/DataHistory/'+ Date + '.txt'
             with open(filename,'a+') as f:
                 f.write(Time.strftime("%X") + ',')
                 f.write(line.decode('utf-8'))
-            with open('/home/pi/Documents/Research/current.txt','w') as f1:
+            with open('/home/pi/Documents/Research/current2.txt','w') as f1:
                 f1.write(line.decode('utf-8'))
-               
+            #sudo apt-get install libffi-dev
+            #pip install scp   
             ssh = paramiko.SSHClient()
             ssh.load_system_host_keys()
             #ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -40,7 +41,7 @@ with serial.Serial( comport , 9600, timeout = 0.001) as arduino:
             
                # SCPCLient takes a paramiko transport as its only argument
             scp= SCPClient(ssh.get_transport()) 
-            scp.put('/home/pi/Documents/Research/current.txt', '/var/www/html/current.txt')
+            scp.put('/home/pi/Documents/Research/current2.txt', '/var/www/html/current2.txt')
             #print '2'
             scp.close()
             #os.system('scp /home/pi/Documents/Research/current.txt joy@64.227.11.204:/var/www/html/current.txt')
